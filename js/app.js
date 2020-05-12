0a7b2277a03b9a7c58cec55bc5bf2d461afea02d
Jhon Gomez Marín <gmezjhon@gmail.com>
2020-05-12 17:55:14 -0500

    Agregamos funcion que compra el curso, delegation y function que lee datos

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 /*
     Variables
 */
-const carrito       = document.getElementById('carrito');
+const carrito  = document.getElementById('carrito');
 const cursos   = document.getElementById('lista-cursos');
 
 
@@ -27,5 +27,15 @@ function cargarEventListeners() {
 // Function que compra el curso
 function comprarCurso(e) {
     e.preventDefault();
-    console.log(e.target);
+    // Con este condicional hacemos de que si contiene esta clase no traiga su elemento padre
+    if (e.target.classList.contains('agregar-carrito')) {
+        const nombreCurso = e.target.parentElement.parentElement;
+        // Esta funcion nos va a mostrar que info tiene
+        leerInfoCurso(nombreCurso);
+    }
+}
+
+// Leer datos del curso
+function leerInfoCurso(nombreCurso) {
+    console.log(nombreCurso)
 }
